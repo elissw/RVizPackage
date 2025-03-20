5403c101589a7006ab3b572cb9579c8d4a27e701
elissw <elisso.stam@gmail.com>
2025-03-20 15:22:38 +0100

    Log binwidth attempt 2

--- R/Useful_functions.R
+++ R/Useful_functions.R
@@ -15,5 +15,5 @@ freedman_diaconis_binwidth_log <- function(x) {
   x <- log10(na.omit(x))  # Apply log transformation
   iqr_x <- IQR(x, na.rm = TRUE)
   n <- length(x)
-  return(10^(2 * iqr_x / (n^(1/3))))  # Convert back to linear scale
+  return(2 * iqr_x / (n^(1/3)))  # Convert back to linear scale
 }
