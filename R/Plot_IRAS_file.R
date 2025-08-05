024dc9733cf241b128922148f1fdb3a8a0bf5ac3
elissw <elisso.stam@gmail.com>
2025-08-05 15:19:19 +0200

    add label to iras plot

--- R/Plot_IRAS_file.R
+++ R/Plot_IRAS_file.R
@@ -16,6 +16,9 @@ plot_IRAS <- function(filename, filetype) {
   library(dplyr)
   library(patchwork)
 
+  # Create dummy dataframe for annotations
+  dummy <- data.frame()
+
   # set the proper theme
   theme_set(theme_professional())
 
@@ -25,17 +28,27 @@ plot_IRAS <- function(filename, filetype) {
 
   if (filetype=="items") {
     plot <- ggplot(df,aes(x=item_IRAS,y=IRAS))
+    # Add column with IRAS ratio
+    df <- df |> mutate(IRAS_ratio = IRAS/item_IRAS)
   }
   if (filetype=="packages") {
     plot <- ggplot(df,aes(x=package_IRAS,y=IRAS))
+    # Add column with IRAS ratio
+    df <- df |> mutate(IRAS_ratio = IRAS/package_IRAS)
   }
+
+  label <- mean(df$IRAS_ratio)
 
   plot <- plot +
     geom_point()+
     geom_abline(intercept = 0, slope=1,
                 color="firebrick", linewidth=0.8,linetype="dashed")+
     scale_x_log10()+scale_y_log10()+
-    labs(x="ActiWiz IRAS",y="Transfer Functions IRAS")
+    labs(x="ActiWiz IRAS",y="Transfer Functions IRAS")+
+    geom_text(data=dummy,
+              aes(x=1e1,y=1e-1),
+              label=paste0("TF/AW = ",round(label,2)))
+
 
   return(plot)
 
