604831f267be02bfe8e82bddb7be18492976b1b7
elissw <elisso.stam@gmail.com>
2025-08-05 14:58:42 +0200

    Add IRAS funciton

---
+++ R/Plot_IRAS_file.R
@@ -0,0 +1,38 @@
+#' Plot ActiWiz IRAS vs calculated IRAS
+#'
+#' Function to plot (almost) the relationship of IRAS
+#' @name plot_IRAS
+#' @param filename Where is the items file located and what's the name
+#' @param filetype "items" or "packages" depending on the file you plot
+#' @export
+
+library(ggplot2)
+library(dplyr)
+library(patchwork)
+
+plot_IRAS <- function(filename, filetype) {
+
+  library(ggplot2)
+  library(dplyr)
+  library(patchwork)
+
+  df <- read.csv(filename)
+
+  plot <- ggplot()
+
+  if (filetype=="items") {
+    plot <- ggplot(df,aes(x=item_IRAS,y=IRAS))
+  }
+  if (filetype=="packages") {
+    plot <- ggplot(df,aes(x=package_IRAS,y=IRAS))
+  }
+
+  plot <- plot +
+    geom_point()+
+    geom_abline(intercept = 0, slope=1,
+                color="firebrick", linewidth=0.8,linetype="dashed")
+
+  return(plot)
+
+
+}
